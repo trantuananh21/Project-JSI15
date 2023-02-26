cf0c27614936a000d92b611231e51d478b0f259f
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2023-02-26 12:51:53 +0100

    upload

--- sign_up/index.js
+++ sign_up/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
-  import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyAYm6OsGaiE0x8cAAiaX6D_2LLyl7waI64",
@@ -15,12 +15,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const provider = new GoogleAuthProvider();
+  const auth = getAuth();
 
   document.getElementById('submit').addEventListener('click', (e)=>{
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var username = document.getElementById('username').value;
-    const auth = getAuth();
 
     createUserWithEmailAndPassword(auth, email, password)
     .then(() => {
@@ -32,7 +32,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
         const json = JSON.stringify(user);
         localStorage.setItem(username, json);
         alert("Đăng kí thành công. Chào mừng cửa hàng" + ' ' + user.username + ' ' + "đã đến với trang web của chúng tôi");
-
+        window.location.href = "http://127.0.0.1:5501/trang_chinh/index.html"
     })
     .catch((error) => {
         const errorCode = error.code;
@@ -43,26 +43,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
 });
 
 document.getElementById('google').addEventListener('click', (e) =>{
-    const auth = getAuth();
-
     signInWithPopup(auth, provider)
   .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
     const user = result.user;
-    console.log(user);
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+    window.location.href = "http://127.0.0.1:5501/trang_chinh/index.html"
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
     const email = error.customData.email;
-    // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
   });
 })
+
+// document.getElementById('sign-out').addEventListener('click', (e) =>{
+//     signOut(auth).then(() => {
+//         window.location.href = "http://127.0.0.1:5501/homepage/index.html"
+//       }).catch((error) => {
+//         console.error(error);
+//       });
+// })
